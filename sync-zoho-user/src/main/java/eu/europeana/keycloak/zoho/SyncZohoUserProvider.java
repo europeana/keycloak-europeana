159d592e41b7561db64555c6ce7a6ad08e77f66a
Lúthien <luthien@parendili.org>
2024-04-23 17:40:10 +0200

    batch zipping and unzipping

--- sync-zoho-user/src/main/java/eu/europeana/keycloak/zoho/SyncZohoUserProvider.java
+++ sync-zoho-user/src/main/java/eu/europeana/keycloak/zoho/SyncZohoUserProvider.java
@@ -1,9 +1,12 @@
 package eu.europeana.keycloak.zoho;
 
 import eu.europeana.api.common.zoho.ZohoConnect;
+
 import java.util.Map;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.jboss.logging.Logger;
@@ -19,7 +22,7 @@ import org.keycloak.utils.StringUtil;
  */
 public class SyncZohoUserProvider implements RealmResourceProvider {
 
-    private static final Logger LOG        = Logger.getLogger(SyncZohoUserProvider.class);
+    private static final Logger LOG = Logger.getLogger(SyncZohoUserProvider.class);
     private static final String LOG_PREFIX = "KEYCLOAK_EVENT:";
     private static final String SUCCESS_MSG = "Success";
     private static final String FAIL_MSG = "Failure";
@@ -33,10 +36,10 @@ public class SyncZohoUserProvider implements RealmResourceProvider {
 
 
     public SyncZohoUserProvider(KeycloakSession session) {
-        this.session      = session;
-        this.realm        = session.getContext().getRealm();
+        this.session = session;
+        this.realm = session.getContext().getRealm();
         this.userProvider = session.users();
-        this.userManager  = new UserManager(session);
+        this.userManager = new UserManager(session);
     }
 
     @Override
@@ -49,42 +52,41 @@ public class SyncZohoUserProvider implements RealmResourceProvider {
      *
      * @return String (completed message)
      */
+
+//    @DefaultValue("1") @QueryParam("age") int minimumAgeInDays) {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public String zohoSync() {
+    public String zohoSync(
+            @DefaultValue("") @QueryParam("job") String job) {
         LOG.info("ZohoSync called.");
         String InstituteJobID;
 
-
-        if (zohoConnect.getOrCreateAccessToZoho()){
-            ZohoBulkJob zohoBulkJob = new ZohoBulkJob();
-            ZohoBulkDownload ZzhoBulkDownload = new ZohoBulkDownload();
-            try {
-//                InstituteJobID = zohoBulkJob.ZohoBulkCreateJob("Accounts");
-//                Thread.sleep(4000);
-                ZohoBulkDownload.downloadResult(Long.valueOf("486281000003484011"));
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                LOG.info("Message: " + e.getMessage() + "; cause: " + e.getCause());
-                return "Zoho is messing up again.";
+        if (zohoConnect.getOrCreateAccessToZoho()) {
+            if (StringUtil.isBlank(job)) {
+                ZohoBulkJob zohoBulkJob = new ZohoBulkJob();
+                try {
+                    InstituteJobID = zohoBulkJob.ZohoBulkCreateJob("Accounts");
+                    //                Thread.sleep(4000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    LOG.info("Message: " + e.getMessage() + "; cause: " + e.getCause());
+                    return "Zoho didn't like that at all.";
+                }
+                return "JobID: " + InstituteJobID;
+            } else {
+                try {
+                    ZohoBulkDownload zohoBulkDownload = new ZohoBulkDownload();
+                    zohoBulkDownload.downloadResult(Long.valueOf(job));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    LOG.info("Message: " + e.getMessage() + "; cause: " + e.getCause());
+                    return "Zoho is messing up. Again.";
+                }
             }
-
-//            ZohoContacts zohoContacts = new ZohoContacts();
-//            try {
-//                lookupUserModel(zohoContacts.getContacts(from, to), pages);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//                LOG.info("Message: " + e.getMessage() + "; cause: " + e.getCause());
-//                return "Grote grutt'n da's niet zo koel ofniedan! ";
-//            }
+            return "Done";
         }
-        return "Krek, da's allebarstes mooi zo.";
+        return "Could not connect to Zoho.";
     }
-
-
-
 
 
     /**
@@ -122,32 +124,29 @@ public class SyncZohoUserProvider implements RealmResourceProvider {
 //        }
 //        return "Krek, da's allebarstes mooi zo.";
 //    }
-
-
-
-    private void lookupUserModel(Map<String, String> results, int pages){
+    private void lookupUserModel(Map<String, String> results, int pages) {
         int usersNotInKeycloak = 0;
         int usersInKeycloak = 0;
         int userInKCAndAffiliated = 0;
         int notAffiliated = 0;
-        for (Map.Entry<String,String> contactAffiliation : results.entrySet()){
-            if (userProvider.getUserByEmail(realm, contactAffiliation.getKey()) == null){
+        for (Map.Entry<String, String> contactAffiliation : results.entrySet()) {
+            if (userProvider.getUserByEmail(realm, contactAffiliation.getKey()) == null) {
                 LOG.info(contactAffiliation.getKey() + " NOT in KC");
-                usersNotInKeycloak ++;
+                usersNotInKeycloak++;
             } else {
-                usersInKeycloak ++;
-                if (StringUtil.isNotBlank(contactAffiliation.getValue())){
+                usersInKeycloak++;
+                if (StringUtil.isNotBlank(contactAffiliation.getValue())) {
                     LOG.info(contactAffiliation.getKey() + " in KC and affiliated with " + contactAffiliation.getValue());
-                    userInKCAndAffiliated ++;
+                    userInKCAndAffiliated++;
                 } else {
                     LOG.info(contactAffiliation.getKey() + " in KC but not affiliated");
-                    notAffiliated ++;
+                    notAffiliated++;
                 }
             }
 
         }
         LOG.info("Summary: in " + pages + " pages, " + results.size() + " contacts with an Institute in Zoho: " + usersNotInKeycloak + " NOT in Keycloak "
-                 + usersInKeycloak + " in Keycloak; of those, " + userInKCAndAffiliated + " are affiliated, " + notAffiliated + "are not.");
+                + usersInKeycloak + " in Keycloak; of those, " + userInKCAndAffiliated + " are affiliated, " + notAffiliated + "are not.");
     }
 
     @Override
--- sync-zoho-user/src/main/java/eu/europeana/keycloak/zoho/ZohoBulkDownload.java
+++ sync-zoho-user/src/main/java/eu/europeana/keycloak/zoho/ZohoBulkDownload.java
@@ -16,12 +16,24 @@ import java.util.Arrays;
 import java.util.Map;
 import org.apache.commons.io.FileUtils;
 
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by luthien on 22/04/2024.
  */
 public class ZohoBulkDownload {
 
-    public static void downloadResult(Long jobId) throws Exception {
+    public void downloadResult(Long jobId) throws Exception {
         BulkReadOperations           bulkReadOperations = new BulkReadOperations();
         APIResponse<ResponseHandler> response           = bulkReadOperations.downloadResult(jobId);
         if (response != null) {
@@ -40,10 +52,13 @@ public class ZohoBulkDownload {
                     InputStream inputStream = streamWrapper.getStream();
                     try {
                         FileUtils.copyInputStreamToFile(inputStream, file);
+                        extractZip(file.getCanonicalPath());
                     } catch (Exception e) {
                         System.out.println("Balen.");
                     }
+
                     inputStream.close();
+
                 } else if (responseHandler instanceof APIException) {
                     APIException exception = (APIException) responseHandler;
                     System.out.println("Status: " + exception.getStatus().getValue());
@@ -65,5 +80,43 @@ public class ZohoBulkDownload {
             }
         }
     }
+
+    private static void extractZip(String canonicalPath) {
+        Path cPath = Paths.get(canonicalPath);
+        Path parentDir = cPath.getParent();
+        String fileName = cPath.toFile().getName();
+//        Path targetDir = parentDir.resolve(FilenameUtils.removeExtension(fileName));
+
+        ArchiveStreamFactory archiveStreamFactory = new ArchiveStreamFactory();
+
+        try (InputStream inputStream = Files.newInputStream(cPath);
+             ArchiveInputStream archiveInputStream = archiveStreamFactory
+                     .createArchiveInputStream(ArchiveStreamFactory.ZIP, inputStream)) {
+
+            ArchiveEntry archiveEntry = null;
+            while ((archiveEntry = archiveInputStream.getNextEntry()) != null) {
+                Path path = Paths.get(parentDir.toString(), archiveEntry.getName());
+                File file = path.toFile();
+                if (archiveEntry.isDirectory()) {
+                    if (!file.isDirectory()) {
+                        file.mkdirs();
+                    }
+                } else {
+                    File parent = file.getParentFile();
+                    if (!parent.isDirectory()) {
+                        parent.mkdirs();
+                    }
+                    try (OutputStream outputStream = Files.newOutputStream(path)) {
+                        IOUtils.copy(archiveInputStream, outputStream);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ArchiveException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
