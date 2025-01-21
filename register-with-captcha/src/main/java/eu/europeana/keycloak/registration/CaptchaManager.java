36238343aeebcdf40dda13432f9600c8999cc9b8
Shwetambara Nazare <nshweta90@gmail.com>
2025-01-21 10:13:56 +0100

    Initial Changes for EA-4012

---
+++ register-with-captcha/src/main/java/eu/europeana/keycloak/registration/CaptchaManager.java
@@ -0,0 +1,101 @@
+package eu.europeana.keycloak.registration;
+
+import static eu.europeana.keycloak.registration.config.CaptachaManagerConfig.secret;
+import static eu.europeana.keycloak.registration.config.CaptachaManagerConfig.verificationUrlHost;
+import static eu.europeana.keycloak.registration.config.CaptachaManagerConfig.verificationUrlPath;
+import static eu.europeana.keycloak.registration.config.CaptachaManagerConfig.verificationUrlScheme;
+
+
+import eu.europeana.keycloak.registration.exception.CaptchaException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.jboss.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+public class CaptchaManager {
+    private static final Logger LOG = Logger.getLogger(CaptchaManager.class);
+
+    private CloseableHttpClient httpClient;
+    @PostConstruct
+    public void init() {
+        httpClient = HttpClients.createDefault();
+    }
+
+    /**
+     * Verify Captcha token by sending request to the verification URL. Response is a JSON with
+     * a field "success" indicating true or false. When it's false "error-codes" field contains
+     * reason of failure.     *
+     * @param captchaToken Token to be verified.
+     * @return true when verification successful, false when there was problem with verification response
+     */
+    public boolean verifyCaptchaToken(String captchaToken)  {
+        String verificationResponse = getVerificationResponse(captchaToken);
+        LOG.debug("Captcha verification response = " + verificationResponse);
+        if (verificationResponse != null) {
+            JSONObject jsonObject = new JSONObject(verificationResponse);
+            if (!jsonObject.getBoolean("success")) {
+                JSONArray jsonArray = jsonObject.getJSONArray("error-codes");
+                LOG.error("Captcha verification error: " + jsonArray.get(0));
+                throw new CaptchaException(jsonArray.get(0).toString());
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Post token in the verification request to the verification URL. Return JSON response or null in case of any exception.
+     *
+     * @param captchaToken token to be verified
+     * @return JSON response from the verification URL or null in case of any exception
+     */
+    private String getVerificationResponse(String captchaToken) {
+        LOG.debug("Sending captcha verification...");
+        try (CloseableHttpResponse response = httpClient.execute(new HttpPost(getVerificationURI(captchaToken)))) {
+            LOG.debug("Received captcha verification");
+            if (response.getStatusLine().getStatusCode() == org.apache.http.HttpStatus.SC_OK) {
+                return IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+            }
+        } catch (URISyntaxException e) {
+            LOG.error("Wrong URI syntax.", e);
+        } catch (IOException e) {
+            LOG.error("Captcha verification request failed.", e);
+        }
+        return null;
+    }
+
+
+    /**
+     * Prepare URI for the request to the verification URL.
+     *
+     * @param captchaToken token to be used as the parameter.
+     * @return URI for the request
+     * @throws URISyntaxException
+     */
+    private URI getVerificationURI(String captchaToken) throws URISyntaxException {
+        URIBuilder builder = new URIBuilder();
+        builder.setScheme(verificationUrlScheme).setHost(verificationUrlHost).setPath(verificationUrlPath)
+                .setParameter("secret", secret)
+                .setParameter("response", captchaToken);
+        return builder.build();
+    }
+
+    @PreDestroy
+    public void close() {
+        try {
+            httpClient.close();
+        } catch (IOException e) {
+            LOG.warn("Closing httpClient failed", e);
+        }
+    }
+}
--- register-with-captcha/src/main/java/eu/europeana/keycloak/registration/RegistrationService.java
+++ register-with-captcha/src/main/java/eu/europeana/keycloak/registration/RegistrationService.java
@@ -1,17 +1,28 @@
 package eu.europeana.keycloak.registration;
 
+import static eu.europeana.keycloak.registration.config.CaptachaManagerConfig.CAPTCHA_MISSING;
+import static eu.europeana.keycloak.registration.config.CaptachaManagerConfig.CAPTCHA_PATTERN;
+import static eu.europeana.keycloak.registration.config.CaptachaManagerConfig.CAPTCHA_VERIFICATION_FAILED;
+
+import eu.europeana.keycloak.registration.exception.CaptchaException;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpHeaders;
 import org.jboss.logging.Logger;
+import org.keycloak.email.EmailException;
+import org.keycloak.http.HttpRequest;
 import org.keycloak.models.ClientModel;
 import org.keycloak.models.ClientProvider;
 import org.keycloak.models.KeycloakSession;
@@ -40,33 +51,82 @@ public class RegistrationService {
   @Path("")
   @POST
   @Consumes({MediaType.APPLICATION_JSON})
+  @Produces({MediaType.TEXT_HTML})
   public Response registerWithCaptcha(RegistrationInput input){
     LOG.info("Input :" + input+ " Headers: " + session.getContext().getHttpRequest().getHttpHeaders());
-    if(input!=null && StringUtils.isNotEmpty(input.getEmail())){
-      UserModel user =getUserBasedOnEmail(input.getEmail());
-      if(user !=null) {
-        LOG.info(" Username for the provided email is : " + user.getUsername());
-        ClientModel client =  getClientAssociatedToUser(user);
-        if(client == null)  {
-          client = createClientWithNewKey(user);
-          RoleModel newRoleForClient = createNewRoleForClient(client);
-          LOG.info("Role created " + newRoleForClient.getName());
-          user.grantRole(newRoleForClient);
+    try {
+      verifyCaptcha();
+      if (input != null && StringUtils.isNotEmpty(input.getEmail())) {
+        UserModel user = getUserBasedOnEmail(input.getEmail());
+        if (user != null) {
+          createClientIfRequiredAndUpdateUser(user);
+          //send mail to accepted email id
+          MailService mailService = new MailService(session,user);
+          mailService.sendEmail();
+
+          return Response.ok().entity("Created new Client, Role for user ").build();
+        } else {
+          return Response.status(Status.BAD_REQUEST).entity(
+              String.format("An Europeana account was not found associated to the email address %s",
+                  input.getEmail())).build();
         }
-        //validate new user
-        //send mail to accepted email id
-        return Response.ok().entity("Created new Client, Role for user " ).build();
       }
-      else {
-        return Response.status(Status.BAD_REQUEST).entity(String.format("An Europeana account was not found associated to the email address %s",input.getEmail())).build();
-      }
+      return Response.status(Status.BAD_REQUEST).entity("The email field is missing").build();
     }
-    return Response.status(Status.BAD_REQUEST).entity("The email field is missing").build();
+    catch (CaptchaException ex){
+      return Response.status(Status.BAD_REQUEST).entity(ex.getMessage()).build();
+    }
+    catch (EmailException ex){
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+    }
   }
+
+  private void createClientIfRequiredAndUpdateUser(UserModel user) {
+    LOG.info(" Username for the provided email is : " + user.getUsername());
+    ClientModel client = getClientAssociatedToUser(user);
+    if (client == null) {
+      client = createClientWithNewKey(user);
+      RoleModel newRoleForClient = createNewRoleForClient(client);
+      LOG.info("New Role : " + newRoleForClient.getName() + " and new client : " + client.getClientId() + " is created for user : " + user.getUsername());
+      user.grantRole(newRoleForClient);
+    }
+  }
+
+  private void verifyCaptcha() {
+    String captchaToken = getAuthorizationHeader(session.getContext().getHttpRequest(), CAPTCHA_PATTERN);
+    if (captchaToken == null) {
+      LOG.info(CAPTCHA_MISSING);
+      throw new CaptchaException(CAPTCHA_MISSING);
+    }
+    CaptchaManager captchaManager = new CaptchaManager();
+    if (!captchaManager.verifyCaptchaToken(captchaToken)) {
+      LOG.info(CAPTCHA_VERIFICATION_FAILED);
+      throw new CaptchaException(CAPTCHA_VERIFICATION_FAILED);
+    }
+  }
+
+  private String getAuthorizationHeader(HttpRequest httpRequest, String captchaPattern) {
+
+      String authorization = httpRequest.getHttpHeaders().getHeaderString(HttpHeaders.AUTHORIZATION);
+      if (authorization != null) {
+        try {
+          Pattern pattern = Pattern.compile(captchaPattern);
+          Matcher matcher = pattern.matcher(authorization);
+
+          if (matcher.find()) {
+            return matcher.group(1);
+          }
+        } catch (RuntimeException e) {
+          LOG.error("Regex problem while parsing authorization header", e);
+        }
+      }
+      return null;
+    }
+
 
   private RoleModel createNewRoleForClient(ClientModel client) {
     RoleProvider roles = session.roles();
-    return  roles.addClientRole(client, "client_owner");
+    return  roles.addClientRole(client, CLIENT_OWNER);
   }
 
   private ClientModel createClientWithNewKey(UserModel user) {
@@ -107,7 +167,6 @@ public class RegistrationService {
     filter.put(UserModel.EMAIL,email);
     Optional<UserModel> userForEmail = userProvider.searchForUserStream(realm, filter).findFirst();
     return userForEmail.orElse(null);
-
   }
 
 }
