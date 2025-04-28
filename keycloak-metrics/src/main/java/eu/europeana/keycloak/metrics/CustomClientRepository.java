c2389a66aa64ad3ea8300c30f71c7d736855e330
srishti <srishti.singh@europeana.eu>
2025-04-28 10:50:08 +0200

    EA-4132 reformat code

--- keycloak-metrics/src/main/java/eu/europeana/keycloak/metrics/CustomClientRepository.java
+++ keycloak-metrics/src/main/java/eu/europeana/keycloak/metrics/CustomClientRepository.java
@@ -1,14 +1,17 @@
 package eu.europeana.keycloak.metrics;
 
 import jakarta.persistence.EntityManager;
+
 public class CustomClientRepository {
-  private final EntityManager em;
-  public CustomClientRepository(EntityManager em) {
-    this.em = em;
-  }
-  public Long findKeyByRoleName(String roleName){
-    String query ="SELECT count(c.id) FROM ClientEntity c ,RoleEntity kr where kr.clientId = c.id and kr.name = :roleNameVal";
-    return em.createQuery(query, Long.class)
-        .setParameter("roleNameVal", roleName).getSingleResult();
-  }
+    private final EntityManager em;
+
+    public CustomClientRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public Long findKeyByRoleName(String roleName) {
+        String query = "SELECT count(c.id) FROM ClientEntity c ,RoleEntity kr where kr.clientId = c.id and kr.name = :roleNameVal";
+        return em.createQuery(query, Long.class)
+                .setParameter("roleNameVal", roleName).getSingleResult();
+    }
 }
--- keycloak-metrics/src/main/java/eu/europeana/keycloak/metrics/UserCountProvider.java
+++ keycloak-metrics/src/main/java/eu/europeana/keycloak/metrics/UserCountProvider.java
@@ -51,6 +51,7 @@ public class UserCountProvider implements RealmResourceProvider {
 
     @Override
     public void close() {
+        // do nothing
     }
 
 
